97e06130a3cdd9a2ec636b667675b2f27220a2ea
Shelcy-Calderon <shelcyjulianacalderon@gmail.com>
2021-07-05 18:49:19 -0500

    Versión 1.0.0 de la aplicación de pedidos

---
+++ src/models/producto.model.ts
@@ -0,0 +1,50 @@
+import {Entity, model, property} from '@loopback/repository';
+// import {Producto} from './producto.model';
+
+@model()
+export class Producto extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  precio: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  imagen: string;
+
+  @property({
+    type: 'string',
+  })
+  productoId?: string;
+
+  @property({
+    type: 'string',
+  })
+  pedidoId?: string;
+
+  constructor(data?: Partial<Producto>) {
+    super(data);
+  }
+}
+
+export interface ProductoRelations {
+  // describe navigational properties here
+}
+
+export type ProductoWithRelations = Producto & ProductoRelations;
